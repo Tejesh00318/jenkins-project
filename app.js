04dbc4f6c156ce7e99f7ac58df74f0829f16d250
Tejesh00318 <tejeshg92@gmail.com>
2026-09-16 11:39:07 +0530

    Update greeting message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Jenkins CI/CD 🚀</h1>');
+  res.send('<h1>Hello from Jenkins CI/CD 🚀 from tejesh</h1>');
 });
 
 app.get('/health', (req, res) => {
